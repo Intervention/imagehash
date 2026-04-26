a5edb03a41cb28a1728623599e011541ef0d7efe
Oliver Vogel <oliver@olivervogel.com>
2026-04-26 09:11:35 +0200

    Add docblocks

--- src/ImageHasher.php
+++ src/ImageHasher.php
@@ -20,26 +20,43 @@ class ImageHasher implements ImageHasherInterface
         //
     }
 
+    /**
+     * Create image hasher statically.
+     */
     public static function create(DriverInterface $driver, StrategyInterface $strategy = new Difference()): self
     {
         return new self($driver, $strategy);
     }
 
+    /**
+     * Create image hasher with given image manipulation driver.
+     */
     public static function usingDriver(DriverInterface $driver): self
     {
         return new self($driver);
     }
 
+    /**
+     * Create a hasher instance with the given driver from the current one.
+     */
     public function withDriver(DriverInterface $driver): self
     {
         return new self($driver, $this->strategy);
     }
 
+    /**
+     * Create a hasher instance with the given strategy from the current one.
+     */
     public function withStrategy(StrategyInterface $strategy): self
     {
         return new self($this->driver, $strategy);
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see ImageHasherInterface::hash()
+     */
     public function hash(mixed $image): HashInterface
     {
         return $this->driver->decodeImage($image)->analyze(new ImageHashAnalyzer($this->strategy));
