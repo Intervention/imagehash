f957067a3168f67277d2cba3dcd3571566cc2205
Oliver Vogel <oliver@olivervogel.com>
2026-07-04 09:10:44 +0200

    Enable ImageHasher to be created with driver string

--- src/ImageHasher.php
+++ src/ImageHasher.php
@@ -4,48 +4,63 @@ declare(strict_types=1);
 
 namespace Intervention\ImageHash;
 
+use Intervention\Image\Exceptions\InvalidArgumentException;
 use Intervention\ImageHash\Analyzers\ImageHashAnalyzer;
 use Intervention\ImageHash\Interfaces\HashInterface;
 use Intervention\ImageHash\Interfaces\ImageHasherInterface;
 use Intervention\ImageHash\Interfaces\StrategyInterface;
 use Intervention\ImageHash\Strategies\Difference;
 use Intervention\Image\Interfaces\DriverInterface;
+use Intervention\Image\Traits\CanResolveDriver;
 
 class ImageHasher implements ImageHasherInterface
 {
+    use CanResolveDriver;
+
+    /**
+     * @throws InvalidArgumentException
+     */
     public function __construct(
-        public DriverInterface $driver,
+        public string|DriverInterface $driver,
         public StrategyInterface $strategy = new Difference(),
     ) {
-        //
+        $this->driver = $this->resolveDriver($driver);
     }
 
     /**
      * Create image hasher statically.
+     *
+     * @throws InvalidArgumentException
      */
-    public static function create(DriverInterface $driver, StrategyInterface $strategy = new Difference()): self
+    public static function create(string|DriverInterface $driver, StrategyInterface $strategy = new Difference()): self
     {
         return new self($driver, $strategy);
     }
 
     /**
      * Create image hasher with given image manipulation driver.
+     *
+     * @throws InvalidArgumentException
      */
-    public static function usingDriver(DriverInterface $driver): self
+    public static function usingDriver(string|DriverInterface $driver): self
     {
         return new self($driver);
     }
 
     /**
      * Create a hasher instance with the given driver from the current one.
+     *
+     * @throws InvalidArgumentException
      */
-    public function withDriver(DriverInterface $driver): self
+    public function withDriver(string|DriverInterface $driver): self
     {
         return new self($driver, $this->strategy);
     }
 
     /**
      * Create a hasher instance with the given strategy from the current one.
+     *
+     * @throws InvalidArgumentException
      */
     public function withStrategy(StrategyInterface $strategy): self
     {
